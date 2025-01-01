68f10b7d51bdf6ef3d04f84b53f992d8aa9a180e
Shubham191297 <thapliyalshubham8@gmail.com>
2025-01-01 11:07:05 +0530

    Updated value of AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_instance" "server" {
-  ami                    = "ami-05d38da78ce859165"
+  ami                    = "ami-0e2c8caa4b6378d8c"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.movieappkey.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
