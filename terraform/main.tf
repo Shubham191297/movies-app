0f71a3b6ddd02190d3a7848d05820cbf9d56b583
Shubham191297 <thapliyalshubham8@gmail.com>
2025-01-01 12:29:51 +0530

    a

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,8 @@ resource "aws_instance" "server" {
 }
 
 resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-ECR-AUTH"
 }
 
 resource "aws_security_group" "maingroup" {
